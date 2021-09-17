c5dca5ba913dc707725a60fa73b47b237cc8be7f
ivandacruz <64236894+ivandacruz@users.noreply.github.com>
2021-09-16 22:00:55 -0300

    Update Area_Direita.java

--- JAVA/Desafios matematicos em java/Area_Direita.java
+++ JAVA/Desafios matematicos em java/Area_Direita.java
@@ -1,27 +1,22 @@
 /*
 Desafios matemáticos em Java
-1 / 3 - Área Direita
-
-
-
-
-
- Básico
- Princípios Básicos
-Desafio
+
+Área Direita
+
+ Básico                                       Princípios Básicos
+
+Desafio:
 Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. 
 Em seguida, calcule e mostre a soma ou a média considerando somente aqueles elementos que estão na 
 área direita da matriz, conforme ilustrado abaixo (área verde).
 
  
-
-
-Entrada
+Entrada:
 A primeira linha de entrada contem um único caractere Maiúsculo O ('S' ou 'M'), 
 indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz. 
 Seguem os 144 valores de ponto flutuante que compõem a matriz.
 
-Saída
+Saída:
 Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
 
  
@@ -36,7 +31,7 @@ S
 
 
 
-(Código dado pelo enunciado)
+Código dado pelo enunciado:
 
 import java.io.IOException;
 import java.util.Scanner;
