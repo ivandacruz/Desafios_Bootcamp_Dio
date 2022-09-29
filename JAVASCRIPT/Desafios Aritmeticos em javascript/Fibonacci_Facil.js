296e863bf11dff0d03cfab88dee710593e013448
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2022-09-29 13:22:16 -0300

    Update Fibonacci_Facil.js

--- JAVASCRIPT/Desafios Aritmeticos em javascript/Fibonacci_Facil.js
+++ JAVASCRIPT/Desafios Aritmeticos em javascript/Fibonacci_Facil.js
@@ -5,7 +5,9 @@ Desafios Aritméticos em JavaScript
 
  Básico                              Princípios Básicos
 
-A seguinte sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como série de Fibonacci. Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores. Escreva um algoritmo que leia um inteiro N (N < 46) e mostre os N primeiros números dessa série.
+A seguinte sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como série de Fibonacci. Nessa sequência, 
+cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores. Escreva um algoritmo que leia um inteiro 
+N (N < 46) e mostre os N primeiros números dessa série.
 
 Entrada
 O arquivo de entrada contém um valor inteiro N (0 < N < 46).
